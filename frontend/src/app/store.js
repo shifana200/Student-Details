ed37dbba9079a8713da9357ada9ab01c7db76f90
shifana200 <shifanasaleem50@gmail.com>
2025-07-02 16:44:14 +0530

    final commit

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import adminReducer from '../features/admin/adminSlice';
-import listSlice from '../features/createSlice';
+
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     admin: adminReducer,
-    todo:listSlice,
+    
 
   },
 });
